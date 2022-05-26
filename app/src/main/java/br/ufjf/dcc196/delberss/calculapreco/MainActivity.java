42dea9342b1be905fcc1d121f62b63ba352b0062
Nícolas Moraes <nicolas.moraes@estudante.ufjf.br>
2022-05-25 22:10:34 -0300

    Implementacao da interface e evento de calcular preco

--- app/src/main/java/br/ufjf/dcc196/delberss/calculapreco/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/delberss/calculapreco/MainActivity.java
@@ -2,13 +2,31 @@ package br.ufjf.dcc196.delberss.calculapreco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.util.ValueIterator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView precoFinal;
+    EditText editTextPreco;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        precoFinal = findViewById(R.id.precoFinal);
+        editTextPreco = findViewById(R.id.editTextPreco);
+
     }
+    public void calcularPreco(View view){
+        Double preco = Double.parseDouble(editTextPreco.getText().toString());
+        precoFinal.setText(preco.toString());
+    }
+
+
 }
