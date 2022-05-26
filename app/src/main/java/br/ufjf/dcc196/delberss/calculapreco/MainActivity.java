23e938e2f3ffedb1de63fb412efad5df0c7299b2
Nícolas Moraes <nicolas.moraes@estudante.ufjf.br>
2022-05-25 22:30:05 -0300

    Implementando o uso dos checkbox e alterando valor do preco final

--- app/src/main/java/br/ufjf/dcc196/delberss/calculapreco/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/delberss/calculapreco/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.icu.util.ValueIterator;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,8 +14,10 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView precoFinal;
-    EditText editTextPreco;
+    private TextView textPrecoFinal;
+    private EditText editTextPreco;
+    private CheckBox checkBoxParaPresente;
+    private CheckBox checkBoxExpresso;
 
 
     @Override
@@ -22,15 +25,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        precoFinal = findViewById(R.id.precoFinal);
+        textPrecoFinal = findViewById(R.id.precoFinal);
         editTextPreco = findViewById(R.id.editTextPreco);
+        checkBoxParaPresente = findViewById(R.id.checkBoxPresente);
+        checkBoxExpresso = findViewById(R.id.checkBoxExpresso);
 
     }
     public void calcularPreco(View view){
+
         Double preco = Double.parseDouble(editTextPreco.getText().toString());
+        Double precoFinal = preco;
+        if (checkBoxParaPresente.isChecked()){
+            precoFinal += 5.00;
+        }
+
+        if (checkBoxExpresso.isChecked()){
+            precoFinal += 12.00;
+        }
 
         Locale locale = new Locale("pt", "BR");
-        precoFinal.setText(NumberFormat.getCurrencyInstance(locale).format(preco));
+        textPrecoFinal.setText(NumberFormat.getCurrencyInstance(locale).format(precoFinal));
     }
 
 
